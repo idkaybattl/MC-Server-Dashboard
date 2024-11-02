2b406b43b393fa904d431f2f4883e542c881b38d
mo <moritz.fechte@gmail.com>
2024-11-02 21:03:40 +0000

    toggling the class for several objects doesnt work yet

--- src/website/js/server-home.js
+++ src/website/js/server-home.js
@@ -1,5 +1,5 @@
 function expand_nav() {
     document.getElementById("nav").classList.toggle("expanded_nav");
     document.getElementById("expand_arrow").classList.toggle("expanded_arrow");
-    document.getElementsByClassName("icon_name").classList.toggle("expanded_icon_name")
+    document.querySelectorAll("icon_name").classList.toggle("expanded_icon_name");
 }
